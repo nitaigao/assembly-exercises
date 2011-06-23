23fcb475b430c07596ded51142a03305047ec0be
nkostelnik <nkostelnik@gmail.com>
2011-06-23 23:49:13 +0100

    given up on linux, working on osx

--- beerbottles/beerbottles.asm
+++ beerbottles/beerbottles.asm
@@ -1,46 +1,53 @@
 section .data
 
-	BOTTLES			equ 100
+  BOTTLES      equ 100
 
 section .bss
-	
-	VERSELEN 	equ 	0
-	VERSE 		resb 	256
-	
+  
+  VERSELEN   equ   0
+  VERSE     resb   256
+  
 
 section .text
 
-        global _start
+  global _start
+  
+_syscall:		
+  int	0x80		;system call
+  ret
 
 _start:
-        nop
+  nop
         
-        mov 	eax, BOTTLES
-		dec 	eax
-		
-loop:	mov 	ebx, 0Ah 			; we will divide by 10
-		mov 	edx, 0				; reset the carry register
-		div 	ebx					; do the division
-		
-		add 	edx, 30h			;take the result and add 48 to it (giving the ascii code)	
-		
-		mov 	[VERSE+esi], edx	; set the buffer offset to the ascii value
-		inc		esi					; increment the offset
-		
-		cmp		eax, 0
-		je		print
-		jmp loop
-		
-print:		
-		mov eax, 0Ah
-		mov [VERSE+esi], eax
-		inc esi
-		mov eax, 4
-		mov ebx, 1
-		mov ecx, VERSE
-		mov edx, esi
-		int 80h
+  mov   eax, BOTTLES
+  dec   eax
+    
+_loop:
+  mov   ebx, 0Ah          ; we will divide by 10
+  mov   edx, 0            ; reset the carry register
+  div   ebx               ; do the division
+  
+  add   edx, 30h          ; take the result and add 48 to it (giving the ascii code) 
+  
+  mov   [VERSE+esi], edx  ; set the buffer offset to the ascii value
+  inc   esi               ; increment the offset
+  
+  cmp   eax, 0            ; see if there are any more numbers to process
+  je    _print            ; if no then print out the string
+  jmp   _loop             ; if yes then loop
+    
+_print:    
+  mov eax, 0Ah
+  mov [VERSE+esi], eax
+  inc esi
+  
+  push  dword esi
+  push  dword VERSE
+  push  dword 1
+  mov   eax, 0x4
+  
+  call _syscall
 
-        mov eax, 1
-        mov ebx, 0
-        int 80H
+  push  dword 0
+  mov   eax, 1
+  call _syscall
