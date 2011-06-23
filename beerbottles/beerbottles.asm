f6314e1a8b469cfe17eed304c8f45d68e726d432
Nick Kostelnik <nk@nk-desktop.(none)>
2011-06-23 22:29:37 +0100

    just about go the number 99 converted to text

--- beerbottles/beerbottles.asm
+++ beerbottles/beerbottles.asm
@@ -1,7 +1,12 @@
 section .data
 
-	VERSE				db	'99 bottles of beer on the wall, 99 bottles of beer.', 10, 'Take one down and pass it around, 98 bottles of beer on the wall.', 10
-	VERSELENGTH			equ	$-VERSE
+	BOTTLES			equ 100
+
+section .bss
+	
+	VERSELEN 	equ 	0
+	VERSE 		resb 	256
+	
 
 section .text
 
@@ -10,14 +15,32 @@ section .text
 _start:
         nop
         
-        mov eax, 4 ;write system call
-        mov ebx, 1 ;stdout 
-        mov ecx, VERSE
-        mov edx, VERSELENGTH
-        int 80h
+        mov 	eax, BOTTLES
+		dec 	eax
+		
+loop:	mov 	ebx, 0Ah 			; we will divide by 10
+		mov 	edx, 0				; reset the carry register
+		div 	ebx					; do the division
+		
+		add 	edx, 30h			;take the result and add 48 to it (giving the ascii code)	
+		
+		mov 	[VERSE+esi], edx	; set the buffer offset to the ascii value
+		inc		esi					; increment the offset
+		
+		cmp		eax, 0
+		je		print
+		jmp loop
+		
+print:		
+		mov eax, 0Ah
+		mov [VERSE+esi], eax
+		inc esi
+		mov eax, 4
+		mov ebx, 1
+		mov ecx, VERSE
+		mov edx, esi
+		int 80h
 
         mov eax, 1
         mov ebx, 0
         int 80H
-
-section .bss
