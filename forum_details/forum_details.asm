ab64a7778ba053324f17dff1ad92a077d8f7e457
Nick Kostelnik <nkostelnik@gmail.com>
2011-06-26 19:41:35 +0100

    asks for name, ignores if begins with a space or if empty

--- forum_details/forum_details.asm
+++ forum_details/forum_details.asm
@@ -1,5 +1,17 @@
 section .data
 
+  ; You are LaRoza, aged 20 next year you will be 21, with user id 266234, the next user is 266235.
+
+  ; User name can't begin with spaces, or have no characters in it. It can truncate strings over 20, if needed.
+  ; Age has to be positive and not 0. It doesn't need a maximum, but you are free to make your program logical.
+  ; User ID can't be negative or 0. The maximum I require is 999999.
+  ; Typing "exit" should exit the program gracefully.
+  ; The prompt should be logical.
+  ; Most importantly, it must not crash, segfault, lockup, or enter an infinite loop no matter what is entered
+
+  FIRSTQ db "Whats your name?: "
+  FIRSTQLEN equ $-FIRSTQ
+
 section .bss
 
   INPUTBUFFLEN  equ 1024
@@ -13,12 +25,33 @@ _start:
 
   nop
 
+_askfirstq:
+
+  ; Ask the first question
+
+  mov   eax, 4            ; sys_write
+  mov   ebx, 1            ; stdout
+  mov   ecx, FIRSTQ       ; text
+  mov   edx, FIRSTQLEN    ; length
+  int   80h
+
+  ; Get the response to the first question
+
   mov   eax, 3            ; sys_read
   mov   ebx, 0            ; stdin
   mov   ecx, INPUTBUFF    ; buffer to read in
   mov   edx, INPUTBUFFLEN ; length of the buffer to read
   int   80h
-  mov   esi, eax          ; copy the input length
+  
+  cmp   eax, 1            ; just contains a character return            
+  je    _askfirstq
+
+  mov   esi, [INPUTBUFF]
+  cmp   esi, 0A20h  ; the first character cant be a space
+  je    _askfirstq
+  
+    
+
 
   mov   eax, 4
   mov   ebx, 1
