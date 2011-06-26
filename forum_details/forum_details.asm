d65b91a892daf2d01c1fd443318d57103c30e9bc
Nick Kostelnik <nkostelnik@gmail.com>
2011-06-26 19:00:37 +0100

    reading basic input and repeating it

--- forum_details/forum_details.asm
+++ forum_details/forum_details.asm
@@ -2,6 +2,9 @@ section .data
 
 section .bss
 
+  INPUTBUFFLEN  equ 1024
+  INPUTBUFF:    resb INPUTBUFFLEN
+
 section .text
 
   global _start
@@ -9,6 +12,19 @@ section .text
 _start:
 
   nop
+
+  mov   eax, 3            ; sys_read
+  mov   ebx, 0            ; stdin
+  mov   ecx, INPUTBUFF    ; buffer to read in
+  mov   edx, INPUTBUFFLEN ; length of the buffer to read
+  int   80h
+  mov   esi, eax          ; copy the input length
+
+  mov   eax, 4
+  mov   ebx, 1
+  mov   ecx, INPUTBUFF
+  mov   edx, esi
+  int   80h
 
   mov   eax, 1
   mov   ebx, 0
